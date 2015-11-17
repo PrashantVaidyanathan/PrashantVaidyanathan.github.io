1a5f6465ace6e6875c5c0fa4376076f60cf8b950
PrashantVaidyanathan <prash@bu.edu>
2015-11-17 12:23:29 -0500

    major set of changes

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+
+var timerval = setInterval(function() {changePic()}, 15000)
+function changePic()
+{
+    var profile_pic = document.getElementById('profilepic');
+    /*if (profile_pic.src.match("profilepic1"))
+    {
+        profile_pic.src = "resources/pics/profilepic2.jpg"
+    }
+    else
+    {
+        profile_pic.src = "resources/pics/profilepic1.jpg"
+    }*/
+
+}
